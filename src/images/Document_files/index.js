001c136a3ccc04d1f5fe371a1015503a7675afbb
yurii-krymskiy <yurii.krymskyi@gmail.com>
2023-09-13 16:45:49 +0200

    museum

---
+++ src/images/Document_files/index.js
@@ -0,0 +1,9 @@
+  for (let myAge = 0; myAge <= 5; myAge += 1) {
+    console.log (`I am ${myAge}`);
+  }
+
+// console.log(printNumbers(5));
+
+
+
+// console.log(getLargestExpressionResultOfThree(-3, 5));
